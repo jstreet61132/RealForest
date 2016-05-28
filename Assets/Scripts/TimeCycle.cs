670cc5ab1f8472149a74d63f49605c24bd9e2e22
Joe Streeter <jstreet61132@yahoo.com>
2016-05-28 09:20:15 -0400

    Adding all files
    OK?

---
+++ Assets/Scripts/TimeCycle.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeCycle : MonoBehaviour {
+
+	public float sunSpeed = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (transform.position.y > 0) {		//if daytime, slower
+			transform.RotateAround (Vector3.zero, Vector3.right, sunSpeed * Time.deltaTime);
+			transform.LookAt (Vector3.zero);
+		}
+		else {		//if nightime, skip ahead
+			transform.RotateAround (Vector3.zero, Vector3.right, 100 * Time.deltaTime);
+			transform.LookAt (Vector3.zero);
+		}
+
+
+
+
+	}
+}
